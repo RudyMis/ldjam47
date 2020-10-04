b0cde5167ae010e624f09fcc3bdaf2b9690a9f14
Name Surname <johndoe@example.com>
2020-10-04 11:49:05 +0200

    Camera supports multiple Crossers

--- cam_move.gd
+++ cam_move.gd
@@ -1,12 +1,14 @@
 extends Line2D
 
 export var change = Vector2(0, 0) # When passing from black to white, change will happen. When passing from white to black, opposite change will happen.
+export var visibility = 0 # Debugging.
 var last_contact = "";
 
-signal change_now
+signal change_now(ch)
 
 func _ready():
-	visible = false;
+	visible = visibility;
+	connect("change_now", get_parent().get_parent().get_node("Camera2D"), "cam_move_handler")
 
 func _process(delta):
 	#if ($left_det.get_overlapping_bodies() != [] || $right_det.get_overlapping_bodies() != []):
