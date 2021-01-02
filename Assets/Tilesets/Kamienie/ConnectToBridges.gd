fe2f19dd7e8710d1854cb0cb9381d67c6fdac74c
Mikolaj <wasiak.mikolaj1@gmail.com>
2021-01-02 21:23:29 +0100

    Tło jaskiniowe, x16 bo chyba jeszcze nie było

--- Assets/Tilesets/Kamienie/ConnectToBridges.gd
+++ Assets/Tilesets/Kamienie/ConnectToBridges.gd
@@ -2,14 +2,14 @@ tool
 extends TileSet
 
 const GROUND = 0
-const BRIDGE = 1
+const BACK = 4
 
 var binds = {
-	GROUND : [BRIDGE],
-	BRIDGE : [GROUND]
+	BACK : [GROUND]
 }
 
 func _is_tile_bound(drawn_id, neighbor_id):
 	if drawn_id in binds:
 		return neighbor_id in binds[drawn_id]
+	print(drawn_id)
 	return false
